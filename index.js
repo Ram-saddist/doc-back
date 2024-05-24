abc8786c91e300bab519c133f65c66c81a2889a2
Ram-saddist <nagulavancharavisivaramteja@gmail.com>
2024-05-24 11:54:33 +0530

    updated logic

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ server.use(jsonServer.bodyParser); // Enable parsing of request body
 
 server.post('/patients', (req, res) => {
   const newPatient = req.body;
+  console.log("new patient",newPatient.doctorId, newPatient)
   const doctors = router.db.get('doctors').value();
-
   // Find the associated doctor details using doctorId
   const associatedDoctor = doctors.find(doctor => doctor.id === Number(newPatient.doctorId));
-
   // Add doctor details to the new patient
   if (associatedDoctor) {
     console.log("associate doctor",associatedDoctor)
@@ -28,9 +27,10 @@ server.post('/patients', (req, res) => {
   console.log("final newptient",newPatient)
   // Add the new patient to the patients array
   router.db.get('patients').push(newPatient).write();
-
+  const p=router.db.get('patients').value();
+  console.log("patients details",p)
   // Respond with the newly created patient data
-  res.json(newPatient);
+  res.send("added successfllyu");
 });
 
 server.use(middlewares);
